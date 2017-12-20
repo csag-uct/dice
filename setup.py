a3296871dda98e59acd3e906ecf3df2058c869cc
Christopher Jack <cjack@csag.uct.ac.za>
2017-12-20 10:24:53 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ddice',
+      version='0.1',
+      description='Multi-dimensional data slicing and dicing, data management and serving',
+      url='https://github.com/csag-uct/ddice',
+      author='Christopher Jack',
+      author_email='cjack@csag.uct.ac.za',
+      license='MIT',
+      packages=['ddice'],
+      zip_safe=False)
